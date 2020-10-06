62332d94ff4996d7bc270e65a4db0ad6dd43f134
James Seward <sewardjm@amazon.com>
2020-10-06 12:50:55 +0100

    Update example code

--- example/main.py
+++ example/main.py
@@ -6,13 +6,10 @@ from appconfig_helper import AppConfigHelper
 
 app = FastAPI()
 
-# initialise the AppConfigHelper object
-# this is done outside the lambda_handler so it is done when the Lambda container is started
-# rather than on every request
 appconfig = AppConfigHelper(
-    os.environ.get("AppConfigApp", "DemoApp"),
-    os.environ.get("AppConfigEnvironment", "prod"),
-    os.environ.get("AppConfigProfile", "main"),
+    "DemoApp",
+    "prod",
+    "main",
     15,
 )
 
@@ -22,7 +19,6 @@ def index(input_string):
     if appconfig.update_config():
         print("Received new configuration")
     output_string = input_string
-    print(appconfig.config)
     if appconfig.config.get("transform_reverse", True):
         output_string = "".join(reversed(output_string))
     if appconfig.config.get("transform_allcaps", False):
